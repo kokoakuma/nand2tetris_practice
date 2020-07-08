a14dd0f67378f3a3f6624b6e48722e7e95887bd2
kokoakuma <aikw9101@gmail.com>
2020-07-08 10:11:39 +0900

    10章終了

--- projects/10/JackAnalyzer/jackTokenizer.js
+++ projects/10/JackAnalyzer/jackTokenizer.js
@@ -1,45 +1,45 @@
 const fs = require('fs');
-const path = require('path');
-
+const path = require("path");
 const {
   TOKEN_TYPE,
   KEYWORDS,
   SYMBOLS,
 } = require('./constants');
-const { builtinModules } = require('module');
-const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
 
 class JackTokenizer {
   constructor(filePath) {
-    let fileContent = fs.readFileSync(path.resolve(__dirname, filePath), { encoding: 'utf-8'});
+    let fileContent = fs.readFileSync(path.resolve(__dirname, filePath), {encoding: "utf-8"});
 
+    // delete comments and empty lines
     while (fileContent.indexOf('/*') !== -1) {
       const index = fileContent.indexOf('/*');
       const index2 = fileContent.indexOf('*/');
-      fileContent = fileContent.slice(0, index) + fileContent.slice(index2+2);
+      fileContent = fileContent.slice(0, index) + fileContent.slice(index2 + 2);
     }
     const lines = fileContent.split(/\n/).filter((line) => {
-      return line.trim() !== '' && line.trim().startsWith('//');
-    })
+      return line.trim() !== '' && !line.trim().startsWith('//');
+    });
 
+    // delete comments for each lines
     const linesWithoutComments = lines.map((line) => {
       return line.split('//')[0].trim();
     });
 
+    // parse each lines
     this.tokens = [];
-    const reg = /[\{\}\(\)[\]\.,;\+\-\*\&\|<>=~]/;
+    const reg = /[\{\}\(\)\[\]\.,;\+\-\*\/&\|<>=~]/;
 
     const parserUnit = (unit) => {
       while (unit) {
         if (unit.match(reg)) {
           const index = unit.match(reg).index;
           if (index !== 0) {
-            this.tokens.push(unit.slice(0, index)); // 区切りの前までを入れる
+            this.tokens.push(unit.slice(0, index));
           }
-          this.tokens.push(unit.slice(index, index+1)); // 区切りとなる演算子などを入れる
-          unit = unit.slice(index+1);　// 残りをunitに入れる
+          this.tokens.push(unit.slice(index, index + 1));
+          unit = unit.slice(index + 1);
         } else {
-          this.tokens.push(unit); // もう区切りがない場合、全部入れる
+          this.tokens.push(unit);
           unit = '';
         }
       }
@@ -54,13 +54,13 @@ class JackTokenizer {
           this.tokens.push(line.slice(0, index + 1));
           line = line.slice(index + 1).trim();
         } else if (doubleQuoteIndex !== -1 && spaceIndex !== -1 && doubleQuoteIndex < spaceIndex) {
-          let unit = lien.slice(0, doubleQuoteIndex);
+          let unit = line.slice(0, doubleQuoteIndex);
           parserUnit(unit);
           line = line.slice(doubleQuoteIndex).trim();
         } else if (spaceIndex !== -1) {
           let unit = line.slice(0, spaceIndex);
           parserUnit(unit);
-          line = line.slice(spaceIndex +  1).trim();
+          line = line.slice(spaceIndex + 1).trim();
         } else {
           parserUnit(line);
           line = '';
@@ -73,15 +73,15 @@ class JackTokenizer {
   }
 
   hasMoreTokens() {
-    return this.tokens.length > this.tokenCounter;
+    return this.tokens.length > this.tokenCounter ? true : false;
   }
 
   advance() {
     if (!this.hasMoreTokens()) return;
-    this.tokenCounter += 1;
+    this.tokenCounter = this.tokenCounter + 1;
     this.currentToken = this.tokens[this.tokenCounter];
     return;
-  };
+  }
 
   tokenType() {
     if (Object.values(KEYWORDS).includes(this.currentToken)) {
@@ -90,12 +90,12 @@ class JackTokenizer {
       return TOKEN_TYPE.SYMBOL;
     } else if (this.currentToken.match(/^[0-9]+$/) && Number(this.currentToken) <= 32767) {
       return TOKEN_TYPE.INT_CONST;
-    } else if (this.currentToken.match(/^[a-zA-z_][a-zA-Z0-9_]*$/)) {
-      return TOKEN_TYPE.IDENTIFER;
+    } else if (this.currentToken.match(/^[a-zA-Z_][a-zA-Z0-9_]*$/)) {
+      return TOKEN_TYPE.IDENTIFIER;
     } else if (this.currentToken.match(/^"[^"\n]*"$/)) {
       return TOKEN_TYPE.STRING_CONST;
     } else {
-      throw new Error(`invalid tokenType. currentToke: ${this.currentToken}`)
+      throw new Error(`invalid tokenType. currentToken: ${this.currentToken}`);
     }
   }
 
@@ -109,18 +109,18 @@ class JackTokenizer {
     return this.currentToken;
   }
 
-  identifer() {
-    if(this.tokenType() !== TOKEN_TYPE.IDENTIFER) return;
+  identifier() {
+    if (this.tokenType() !== TOKEN_TYPE.IDENTIFIER) return;
     return this.currentToken;
   }
 
   intVal() {
-    if(this.tokenType() !== TOKEN_TYPE.INT_CONST) return;
+    if (this.tokenType() !== TOKEN_TYPE.INT_CONST) return;
     return this.currentToken;
   }
 
-  stingVal() {
-    if(this.tokenType() !== TOKEN_TYPE.STRING_CONST) return;
+  stringVal() {
+    if (this.tokenType() !== TOKEN_TYPE.STRING_CONST) return;
     return this.currentToken.slice(1, -1);
   }
 };
